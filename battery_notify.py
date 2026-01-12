e489bdd23c17fc20be1846a38d75e16892b7dd3c
Jaiditya-01 <jaidityachauhan04@gmail.com>
2026-01-13 00:25:52 +0530

    phase-2 testing

--- battery_notify.py
+++ battery_notify.py
@@ -2,12 +2,20 @@ import psutil
 from plyer import notification
 import time
 import winsound
+from datetime import datetime
 
-MAX = 85
-INTERVAL = 60
+MAX = 85 # % of battery
+INTERVAL = 60 # seconds of checking
+LOG = "battery_log.txt"
 
-notified = False
+previous = None
 
+# function to write into .txt file
+def log_event(message):
+    with open(LOG, "a") as file:
+        file.write(message + "\n")
+
+# this will continuosly monitor battery while charging
 while True:
     battery = psutil.sensors_battery()
 
@@ -17,23 +25,27 @@ while True:
 
     percent = battery.percent
     plugged = battery.power_plugged
+    current_time = datetime.now.strftime("%Y-%m-%d %H:%M:%S")
 
-    if plugged and percent >= MAX and not notified:
-        try:
-            winsound.Beep(1000, 800)
+    if previous is None:
+        previous = plugged
 
-            notification.notify(
-                title="Battery Alert !",
-                message=f"Battery reached {percent}%. Please unplug the charger.",
-                timeout=10
-            )
+    if plugged != previous:
+        if plugged:
+            log_event(f"[{current_time}] Charger Plugged In")
+        else:
+            log_event(f"[{current_time}] Charger Unplugged")
 
-            notified = True
+        previous = plugged
 
-        except:
-            notified = False 
-
-    if percent <= MAX - 3:
-        notified = False
+    # sent message until unplug the charger
+    if plugged and percent >= MAX:
+        winsound.Beep(1000, 800)
+        
+        notification.notify(
+            title="Battery Alert !",
+            message=f"Battery reached {percent}%. Please unplug the charger.",
+            timeout=10
+        )
 
     time.sleep(INTERVAL)
